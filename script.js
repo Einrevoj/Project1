1b8c30a3ddc92c8d5ae85009ef76574a3013d452
Einrevoj <jovcano28@gmail.com>
2022-08-01 15:44:14 +0800

    updated design

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    // typing animation script
+    var typed = new Typed(".typing",{
+        strings: ["Full-Stack Developer", "Freelancer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true 
+    });
+    var typed = new Typed(".typing-2",{
+        strings: ["Full-Stack Developer", "Freelancer", "Designer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true 
+    });
+});
